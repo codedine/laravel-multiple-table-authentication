a88412b75ddb906fd7bfaa21796ed6e94b3d95f3
codedine <codedine94@gmail.com>
2019-12-12 20:13:53 +0530

    Multiple Auth Implementation

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,31 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::group(['middleware' => ['web'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
+    // Authentication Routes...
+    Route::get('login', 'AdminAuth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'AdminAuth\LoginController@login');
+    Route::post('logout', 'AdminAuth\LoginController@logout')->name('logout');
+
+    // Registration Routes...
+    Route::get('register', 'AdminAuth\RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'AdminAuth\RegisterController@register');
+
+    // Password Reset Routes...
+    Route::get('password/reset', 'AdminAuth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('password/email', 'AdminAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('password/reset/{token}', 'AdminAuth\ResetPasswordController@showResetForm')->name('password.reset');
+    Route::post('password/reset', 'AdminAuth\ResetPasswordController@reset')->name('password.update');
+
+    // Email Verification Routes...
+    Route::get('email/verify', 'AdminAuth\VerificationController@show')->name('verification.notice');
+    Route::get('email/verify/{id}/{hash}', 'AdminAuth\VerificationController@verify')->name('verification.verify');
+    Route::get('email/resend', 'AdminAuth\VerificationController@resend')->name('verification.resend');
+
+    Route::group(['middleware' => ['admin.auth']], function () {
+        Route::get('/', 'AdminController@index')->name('home');
+    });
+
+});
+
 Route::get('/home', 'HomeController@index')->name('home');
